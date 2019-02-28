cab9a60cc16efc074e3a401c2765acdc34ef17a8
chengyu <862226994@qq.com>
2019-02-28 17:59:22 +0800

    fix

--- reptile/index.py
+++ reptile/index.py
@@ -3,9 +3,8 @@
 import os
 import re
 import requests
-
-
 from lxml import etree
+
 
 def get_html(url, name):
     """
@@ -14,19 +13,7 @@ def get_html(url, name):
     :param name: 名称
     :return:
     """
-
-    headers = {
-        'Referer': 'https://www.23us.so/',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
-    }
-
-    r = requests.get(url, headers=headers)
-    status_code = r.status_code
-    if status_code != 200:
-        exit(0)
-
-    r.encoding = 'utf-8'
-    html = etree.HTML(r.text)
+    html = get_resource(url)
     content = html.xpath('//dd[@id="contents"]')
     c = etree.tostring(content[0], encoding='utf-8', method='text').decode('utf-8')
     c = re.sub("(\n+)|(\r\n)+|(<br>)+", '', c)
@@ -44,6 +31,7 @@ def write_content_to_file(file_name, file_data):
     file.write(file_data)
     file.close()
 
+
 def mkdir(path):
     """
     新建文件夹
@@ -57,12 +45,29 @@ def mkdir(path):
     else:
         print("目录:" + str(path) + "已存在")
 
+
 def get_index():
     """
     获取文章所有章节
     :return:
     """
-    url =  'https://www.23us.so/files/article/html/2/2521/index.html'
+    url = 'https://www.23us.so/files/article/html/2/2521/index.html'
+    html = get_resource(url)
+    content = html.xpath('//td[@class="L"]')
+    mkdir('儒道至圣')
+    for key, index in enumerate(content):
+        c = etree.tostring(index, encoding='utf-8', method='html').decode('utf-8')
+        lists = re.findall(r'<td class="L"><a href="(.+?)">(.+?)</a></td>', c)
+        for href, name in lists:
+            get_html(href, name)
+
+
+def get_resource(url):
+    """
+    获取资源
+    :param url:
+    :return:
+    """
     headers = {
         'Referer': 'https://www.23us.so/',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
@@ -75,13 +80,7 @@ def get_index():
 
     r.encoding = 'utf-8'
     html = etree.HTML(r.text)
-    content = html.xpath('//td[@class="L"]')
-    for key, index in enumerate(content):
-        str = etree.tostring(index, encoding='utf-8', method='html').decode('utf-8')
-        list = re.findall(r'<td class="L"><a href="(.+?)">(.+?)</a></td>', str)
-        for href, name in list:
-            get_html(href, name)
-
+    return html
 
 
 get_index()
